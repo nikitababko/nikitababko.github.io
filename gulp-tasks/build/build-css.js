5d5b59208647188efdeb7f41f1378f3b912483e5
Nikita Babko <niki.babko@gmail.com>
2022-10-03 15:12:52 +0700

    1) Added husky; 2) Updated gulp-sass

--- gulp-tasks/build/build-css.js
+++ gulp-tasks/build/build-css.js
@@ -1,15 +1,16 @@
 module.exports = function (gulp, plugins) {
-    return async function () {
-        gulp.src("./src/css/index.css")
-            .pipe(plugins.sourcemaps.init())
-            .pipe(
-                plugins.sass({
-                    errorLogToConsole: true,
-                    outputStyle: "compressed", // minify file
-                })
-            )
-            .on("error", console.error.bind(console))
-            .pipe(plugins.sourcemaps.write("./"))
-            .pipe(gulp.dest("./build/css/"));
-    };
+  return async function () {
+    gulp
+      .src('./src/css/index.css')
+      .pipe(plugins.sourcemaps.init())
+      .pipe(
+        plugins.dartSass({
+          errorLogToConsole: true,
+          outputStyle: 'compressed', // minify file
+        }),
+      )
+      .on('error', console.error.bind(console))
+      .pipe(plugins.sourcemaps.write('./'))
+      .pipe(gulp.dest('./build/css/'));
+  };
 };
